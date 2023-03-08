7a8f19f91bea38bd39ea274fa0a5f1b2e8620fdb
22102416 <pedrovs3@hotmail.com>
2023-03-08 07:55:00 -0300

    add insomnia routes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,3 @@
-import { number } from 'zod';
 import { prisma } from '../src/v1/lib/prisma';
 
 const date = new Date(Date.now());
@@ -10,187 +9,193 @@ async function main() {
 
   await prisma.gender.createMany({
     data: [
-      { name: 'Feminino', abbreviation: 'F' },
-      { id: 'teste', name: 'Masculino', abbreviation: 'M' },
+      {
+        name: 'Feminino',
+        abbreviation: 'F',
+      },
+      {
+        name: 'Masculino',
+        abbreviation: 'M',
+      },
     ],
   });
 
   // @ts-ignore
-  const user1 = await prisma.user.create({
-    data: {
-      name: 'Pedro Vieira',
-      birthdate: date,
-      cpf: '48937257501',
-      email: 'Teste@hotmail.com',
-      password: 'teste123',
-      // @ts-ignore
-      userAddress: {
-        create: {
-          address: {
-            create: {
-              number: '146',
-              postal_code: '06150-130',
-            },
-          },
-        },
-      },
-      tbl_user_phone: {
-        create: {
-          tbl_phone: {
-            create: {
-              number: '123141234132',
-            },
-          },
-        },
-      },
-      gender: {
-        connect: {
-          id: 'teste',
-        },
-      },
-    },
-  });
-
-  const user2 = await prisma.user.create({
-    data: {
-      name: 'Enzo Prado',
-      birthdate: date,
-      cpf: '27489175414',
-      email: 'enzodp@gmail.com',
-      password: 'teste123',
-      // @ts-ignore
-      userAddress: {
-        create: {
-          address: {
-            create: {
-              number: '146',
-              postal_code: '06150-130',
-            },
-          },
-        },
-      },
-      gender: {
-        connect: {
-          id: 'teste',
-        },
-      },
-    },
-  });
-
-  const user3 = await prisma.user.create({
-    data: {
-      name: 'Gui Japo',
-      birthdate: date,
-      cpf: '48950273495',
-      email: 'drpixelss@gmail.com',
-      password: 'teste123',
-      // @ts-ignore
-      userAddress: {
-        create: {
-          address: {
-            create: {
-              number: '146',
-              postal_code: '06150-130',
-            },
-          },
-        },
-      },
-      gender: {
-        connect: {
-          id: 'teste',
-        },
-      },
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: 'Teste@hotmail.com',
-    },
-    select: {
-      name: true,
-      email: true,
-      // @ts-ignore
-      userAddress: {
-        select: {
-          address: {
-            select: {
-              number: true,
-              postal_code: true,
-            },
-          },
-        },
-      },
-      cpf: true,
-      id: true,
-      gender: {
-        select: {
-          abbreviation: true,
-        },
-      },
-      tbl_user_phone: {
-        select: {
-          tbl_phone: {
-            select: {
-              number: true,
-            },
-          },
-        },
-      },
-    },
-  });
-
-  const ngo = await prisma.nGO.create({
-    data: {
-      name: 'Teste ngo',
-      email: 'test@test.com',
-      password: '4u10491',
-      cnpj: '4029402104',
-      tbl_ngo_address: {
-        create: {
-          tbl_address: {
-            create: {
-              number: '146',
-              postal_code: '498124081',
-            },
-          },
-        },
-      },
-    },
-  });
-
-  console.log(ngo);
-
-  const users = await prisma.user.findMany();
-  const userToDelete = await prisma.user.findUnique({
-    where: {
-      email: 'enzodp@gmail.com',
-    },
-    include: {
-      // @ts-ignore
-      userAddress: {
-        include: {
-          address: true,
-        },
-      },
-    },
-  });
-
-  await prisma.address.delete({
-    where: {
-      // @ts-ignore
-      id: userToDelete?.userAddress[0].id_address,
-    },
-  });
-
-  await prisma.user.delete({
-    where: {
-      email: 'enzodp@gmail.com',
-    },
-  });
-  const addresses = await prisma.address.findMany();
-
-  console.log({ user, users, addresses });
+  // const user1 = await prisma.user.create({
+  //   data: {
+  //     name: 'Pedro Vieira',
+  //     birthdate: date,
+  //     cpf: '48937257501',
+  //     email: 'Teste@hotmail.com',
+  //     password: 'teste123',
+  //     // @ts-ignore
+  //     userAddress: {
+  //       create: {
+  //         address: {
+  //           create: {
+  //             number: '146',
+  //             postal_code: '06150-130',
+  //           },
+  //         },
+  //       },
+  //     },
+  //     tbl_user_phone: {
+  //       create: {
+  //         tbl_phone: {
+  //           create: {
+  //             number: '123141234132',
+  //           },
+  //         },
+  //       },
+  //     },
+  //     gender: {
+  //       connect: {
+  //         id: 'teste',
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // const user2 = await prisma.user.create({
+  //   data: {
+  //     name: 'Enzo Prado',
+  //     birthdate: date,
+  //     cpf: '27489175414',
+  //     email: 'enzodp@gmail.com',
+  //     password: 'teste123',
+  //     // @ts-ignore
+  //     userAddress: {
+  //       create: {
+  //         address: {
+  //           create: {
+  //             number: '146',
+  //             postal_code: '06150-130',
+  //           },
+  //         },
+  //       },
+  //     },
+  //     gender: {
+  //       connect: {
+  //         id: 'teste',
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // const user3 = await prisma.user.create({
+  //   data: {
+  //     name: 'Gui Japo',
+  //     birthdate: date,
+  //     cpf: '48950273495',
+  //     email: 'drpixelss@gmail.com',
+  //     password: 'teste123',
+  //     // @ts-ignore
+  //     userAddress: {
+  //       create: {
+  //         address: {
+  //           create: {
+  //             number: '146',
+  //             postal_code: '06150-130',
+  //           },
+  //         },
+  //       },
+  //     },
+  //     gender: {
+  //       connect: {
+  //         id: 'teste',
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // const user = await prisma.user.findUnique({
+  //   where: {
+  //     email: 'Teste@hotmail.com',
+  //   },
+  //   select: {
+  //     name: true,
+  //     email: true,
+  //     // @ts-ignore
+  //     userAddress: {
+  //       select: {
+  //         address: {
+  //           select: {
+  //             number: true,
+  //             postal_code: true,
+  //           },
+  //         },
+  //       },
+  //     },
+  //     cpf: true,
+  //     id: true,
+  //     gender: {
+  //       select: {
+  //         abbreviation: true,
+  //       },
+  //     },
+  //     tbl_user_phone: {
+  //       select: {
+  //         tbl_phone: {
+  //           select: {
+  //             number: true,
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // const ngo = await prisma.nGO.create({
+  //   data: {
+  //     name: 'Teste ngo',
+  //     email: 'test@test.com',
+  //     password: '4u10491',
+  //     cnpj: '4029402104',
+  //     tbl_ngo_address: {
+  //       create: {
+  //         tbl_address: {
+  //           create: {
+  //             number: '146',
+  //             postal_code: '498124081',
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // console.log(ngo);
+  //
+  // const users = await prisma.user.findMany();
+  // const userToDelete = await prisma.user.findUnique({
+  //   where: {
+  //     email: 'enzodp@gmail.com',
+  //   },
+  //   include: {
+  //     // @ts-ignore
+  //     userAddress: {
+  //       include: {
+  //         address: true,
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // await prisma.address.delete({
+  //   where: {
+  //     // @ts-ignore
+  //     id: userToDelete?.userAddress[0].id_address,
+  //   },
+  // });
+  //
+  // await prisma.user.delete({
+  //   where: {
+  //     email: 'enzodp@gmail.com',
+  //   },
+  // });
+  // const addresses = await prisma.address.findMany();
+  //
+  // console.log({ user, users, addresses });
 }
 
 main()
