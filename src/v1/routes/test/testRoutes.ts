34fca330900c8d752d14ae0a736cf6db9cdd2024
22102416 <pedrovs3@hotmail.com>
2023-03-09 11:27:47 -0300

    fix test route register

--- src/v1/routes/test/testRoutes.ts
+++ src/v1/routes/test/testRoutes.ts
@@ -2,5 +2,5 @@ import { FastifyInstance } from 'fastify';
 import homeController from '../../controllers/HomeController';
 
 export function testRoutes(fastify: FastifyInstance) {
-  fastify.get('/users', homeController.index);
+  fastify.get('/users/', homeController.index);
 }
